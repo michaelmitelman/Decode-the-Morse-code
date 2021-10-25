f2e836e08facc9920c852d2db90662adc777abad
michaelmitelman <michaelmitelman10@gmail.com>
2021-10-25 14:05:25 +0300

    decodeBits function Implementation

--- MorseCode.js
+++ MorseCode.js
@@ -42,14 +42,48 @@ const MORSE_CODE_DICT = {
     "--..--":","
 };
 
+const rules = { 'dot': 1, 'dash': 3, 'ddPause': 1, 'charPause': 3, 'wordsPause': 7};
 
 /* decodeBits finds out the transmission rate of the message, correctly decode the message to dots, dashes and spaces,
    and return those as a string. */
    function decodeBits(bits){ 
+         bits.replace(/(^[0]+)|([0]+)$/g, '');
+         const zeroSymbols = bits.match(/[0]+/);
+         const maxTimeUnit = String(Math.max(...bits.match(/[1]+/g))).length;
+         const minTimeUnit = String(Math.min(...bits.match(/[1]+/g))).length;
+         const rate = (len) => time => len * rules[time];
+
+         if (minTimeUnit == maxTimeUnit) {
+             if(zeroSymbols) {
+                 const len = zeroSymbols[0].length;
+                 times = (len == 1 || len == 3 || len == 7) ? rate(1) : rate(len); 
+             } else {
+                 times = rate(minTimeUnit);
+             }
+         } else {
+             times = rate(minTimeUnit);
+         }
+
+         const translationToMorse = word => {
+            return word.replace(binaryRegex(1, 'dot'), '-').replace(binaryRegex(1, 'dash'), '.').replace(binaryRegex(0, 'charPause'), ' ').replace(binaryRegex(0, 'ddPause'), '');
+        };
+        
+        const words = bits.split(printBinary(0, 'wordsPause'));
+        return words.map(translationToMorse).join('   ');
+        }
          
-}
+
 
 // decodeMors takes the output of the previous function and return a human-readable string 
    function decodeMors(morseCode){
        return morseCode.split('       ').map((v) => v.split('   ')).map((v) => v.map((value) => MORSE_CODE_DICT[value.replace(/s/g, '')]).join('')).join(' ');
+   }
+
+
+   function printBinary(binary, time) {
+    return Array.from({length: times(time)}, () => b).join('');
+   }
+
+   function binaryRegex(binary, type) {
+    return new RegExp(printBinary(binary, type), 'g');
    }
